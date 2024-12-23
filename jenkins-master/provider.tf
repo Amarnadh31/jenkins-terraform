8ea7275dcd4c7e49bed7e39f6966b98324900bbf
Amar <cberg2386@gmail.com>
2024-12-23 07:49:18 +0530

    master instance

---
+++ jenkins-master/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws ={
+        source = "hashicorp/aws"
+        version = "~> 5.7"
+    }
+  }
+  backend "s3" {
+        bucket = "demo-bucket-31"
+        key = "jenkins-master"
+        region = "us-east-1"
+        dynamodb_table= "demo_key"    
+    
+  }
+}
+
+provider "aws" {
+    region = "us-east-1"
+  
+}
